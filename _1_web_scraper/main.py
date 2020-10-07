5d8297dd08fe0a12f78eb97558b8e79a48886916
Pepijn K <pepijn@krijnsen.com>
2020-10-07 22:07:35 +0100

    Building dict of lists

--- _1_web_scraper/main.py
+++ _1_web_scraper/main.py
@@ -20,9 +20,32 @@ URL = "https://www.monster.ie/jobs/search/?q=python-developer&cy=ie&stpage=1&pag
 page = requests.get(URL)
 doc = html.fromstring(page.content)
 
-job_title_links = doc.xpath("//*[@id='SearchResults']/section/div/div[2]/header/h2/a/@href")
-job_title_names = doc.xpath("//*[@id='SearchResults']/section/div/div[2]/header/h2/a/text()")
-job_location = doc.xpath("//*[@id='SearchResults']/section/div/div[2]/div[2]/a/text()")
+results = {}
+
+job_title_sections = doc.xpath("//*[@id='SearchResults']/section/@data-jobid")
+
+i = 0
+for section_id in job_title_sections:
+    i += 1
+    job_title_name = doc.xpath("//*[@data-jobid={}]/div/div[2]/header/h2/a/text()".format(section_id))
+    if job_title_name:
+        job_title_links = doc.xpath("//*[@data-jobid={}]/div/div[2]/header/h2/a/@href".format(section_id))
+        job_location = doc.xpath("//*[@id='SearchResults']/section/div/div[2]/div[2]/a/text()")
+        nll_list = []
+        nll_list.append(job_title_name)
+        nll_list.append(job_title_links)
+        nll_list.append(job_location)
+        results[section_id] = nll_list
+    if i == 8:
+        quit()
+
+print(results)
+
+# job_title_links = doc.xpath("//*[@data-jobid={}]/div/div[2]/header/h2/a/@href".format(section_id))
+# job_title_names = doc.xpath("//*[@id='SearchResults']/section/div/div[2]/header/h2/a/text()")
+# job_location = doc.xpath("//*[@id='SearchResults']/section/div/div[2]/div[2]/a/text()")
+
+
 
 intro = """
 ################################
@@ -31,13 +54,13 @@ JOB OVERVIEW
 """
 preferred_county = "Jobs in {}\n".format(county)
 
-i = 0
-for v in job_location:
-    i += 1
-    print(v)
-    if county in v:
-        results = job_title_links[i] + ", " + job_location[i] + "\n"
-
-print(intro)
-print(preferred_county)
-print(results)
+# i = 0
+# for v in job_location:
+#     i += 1
+#     print(v)
+#     if county in v:
+#         results = job_title_links[i] + ", " + job_location[i] + "\n"
+# 
+# print(intro)
+# print(preferred_county)
+# print(results)
